f182032bc636135be42a453c3ce9fd97d4c7f66a
Thakshila Dilrukshi <tdthakshi@gmail.com>
2022-02-08 01:32:12 +0530

    initial changes
    initial changes

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'practice-app',
+  webDir: 'dist/practice-app',
+  bundledWebRuntime: false
+};
+
+export default config;
